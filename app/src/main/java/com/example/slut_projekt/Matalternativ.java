1c8526645a245105b459ff18e012a5dd87d0ba71
a20caros <a20caros@student.his.se>
2021-05-04 08:46:04 +0200

    Ett intent implementerat för hamburgar knappen

--- app/src/main/java/com/example/slut_projekt/Matalternativ.java
+++ app/src/main/java/com/example/slut_projekt/Matalternativ.java
@@ -2,7 +2,10 @@ package com.example.slut_projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Matalternativ extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Matalternativ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matalternativ);
+
+        Button button = findViewById(R.id.Hamburgare_knapp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (Matalternativ.this, Hamburgare.class);
+                startActivity(intent);
+            }
+        });
     }
 }
