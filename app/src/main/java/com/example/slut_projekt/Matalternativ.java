7fa2508334e77911998f73052924064a4bfdcf15
a20caros <a20caros@student.his.se>
2021-05-04 10:23:48 +0200

    Tog bort knapparnas funktioner i Matalternativ

--- app/src/main/java/com/example/slut_projekt/Matalternativ.java
+++ app/src/main/java/com/example/slut_projekt/Matalternativ.java
@@ -41,32 +41,6 @@ public class Matalternativ extends AppCompatActivity {
         }catch (Exception e){
             Log.e("MainActivity ==>","Something went wrong when reading textfile:\n\n"+ e.getMessage());
         }
-
-
-        Button button = findViewById(R.id.Hamburgare_knapp);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (Matalternativ.this, Hamburgare.class);
-                startActivity(intent);
-            }
-        });
-        Button button1 = findViewById(R.id.Pizza_knapp);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (Matalternativ.this, Pizza.class);
-                startActivity(intent);
-            }
-        });
-        Button button2 = findViewById(R.id.Pasta_knapp);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (Matalternativ.this, Pizza.class);
-                startActivity(intent);
-            }
-        });
     }
     @SuppressLint("StaticFieldLeak")
     private class JsonTask extends AsyncTask<String, String, String> {
