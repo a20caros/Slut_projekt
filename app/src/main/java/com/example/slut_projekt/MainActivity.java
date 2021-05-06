0622a0b6e28d4a35b3730800ac2aec4c95c6f3d5
a20caros <a20caros@student.his.se>
2021-05-06 09:12:02 +0200

    Lagt till rätt sida fär att när användaren klickar på om appen knappen

--- app/src/main/java/com/example/slut_projekt/MainActivity.java
+++ app/src/main/java/com/example/slut_projekt/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (MainActivity.this, Matalternativ.class);
+                Intent intent = new Intent (MainActivity.this, About.class);
                 startActivity(intent);
             }
         });
